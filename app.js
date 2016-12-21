a9b95b8044826ebbbb77ad37c86ae1e3367b012d
Daniel Schwen <daniel.schwen@inl.gov>
2016-12-21 15:06:52 -0700

    login routes

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var passport = require('passport');
 
 var app = express();
 
@@ -28,8 +27,13 @@ app.use(require('node-sass-middleware')({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// required for passport
+app.use(session({ secret: process.env.SESSION_SECRET || 'grudanplattformdefaultsecret' }));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+require('./app/routes.js')(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
